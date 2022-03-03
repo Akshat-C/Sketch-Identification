d7acd3ad4518d436adefd50b3634ba452fd6dd22
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-03-03 19:09:54 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 function preload()
-{}
+{
+    classifier = ml5.imageClassifier("Doodlenet");
+}
 
 function setup()
 {
     canvas = createCanvas(450, 350);
     canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
 
 function draw()
-{}
+{
+    strokeWeight(10);
+    stroke(0);
+    if (mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("sketch_name").innerHTML = results[0].label;
+        percentag = results[0].confidence*100;
+        document.getElementById("confidence").innerHTML = percentag.toFixed(2) + " %";
+    }
+}
 
 function clear_canvas()
 {
